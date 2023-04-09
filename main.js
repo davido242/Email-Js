ad279da81e3153ac8f690f44dbb27728590279a8
Monday_David_Sarka <david_md@hamari.agency>
2023-04-09 13:40:44 +0100

    Added js smtp script and functions

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// alert("Please drop us a message fam!!");
+function sendEmail () {
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "davidsaka242@gmail.com",
+        Password : "davido242",
+        To : 'davidsarka242@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : "New Contact Form Enquiries",
+        Body : document.getElementById("message").value,
+    }).then(
+      message => alert(message)
+    );
+    alert("Thanks for for your message");
+}
