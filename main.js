2684e34df734d861a8a19b77ff320ae13ccbd616
Monday_David_Sarka <david_md@hamari.agency>
2023-04-10 15:18:20 +0100

    encrypted username, pwrd, host and made sending successful

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 function sendEmail () {
     Email.send({
-        Host : "smtp.gmail.com",
-        Username : "askMe@gmail.com",
-        Password : "askMe",
-        To : 'askMe@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Enquiries",
-        Body : document.getElementById("message").value,
+        SecureToken: "0d686827-65be-43c5-aa25-91c9a389d70c",
+        To : "davidsaka242@gmail.com",
+        From : "davidsarka242@gmail.com",
+        Subject : "2NGO Test Mail",
+        Body : "Message " + document.getElementById("message").value + "<br /> Name : " + document.getElementById("name").value + 
+        "<br /> Number : " + document.getElementById("phone").value + "<br /> Email : " + document.getElementById("email").value
     }).then(
       message => alert(message)
     );
-    alert("Thanks for for your message");
 }
